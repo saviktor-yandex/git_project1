af235a8642bac6f2aa8b07f3232c09379ab8fda2
saviktor-yandex <94538734+saviktor-yandex@users.noreply.github.com>
2021-11-17 18:18:58 +0500

    Update program.py

--- program.py
+++ program.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     # изменения
-
+    print("отредактировал через сайт")
 
 if __name__ == '__main__':
     main()
